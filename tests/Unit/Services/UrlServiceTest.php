2dcb411d09825de6e4f52221a64df9c9aa660441
John Halsey <johnhalsey82@gmail.com>
2024-07-03 21:29:21 +0100

    Service test

---
+++ tests/Unit/Services/UrlServiceTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use Tests\TestCase;
+use App\Services\UrlService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UrlServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_will_generate_a_unique_short_url()
+    {
+        $urlService = new UrlService();
+
+        $shortUrls = [];
+        for ($i = 0; $i < 100; $i++) {
+            $shortUrls[] = $urlService->generateUniqueUrl();
+        }
+        // assert all the short urls are unique
+        $this->assertEquals(count($shortUrls), count(array_unique($shortUrls)));
+    }
+}
