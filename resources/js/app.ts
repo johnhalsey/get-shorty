55218150722c0e430a62439eac113a9e435eabe1
John Halsey <johnhalsey82@gmail.com>
2024-10-18 09:27:53 +0100

    Create app.ts

---
+++ resources/js/app.ts
@@ -0,0 +1,34 @@
+import '../css/app.css';
+import './bootstrap';
+
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, DefineComponent, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+import Encode from "./Components/Encode.vue";
+import Decode from "./Components/Decode.vue";
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob<DefineComponent>('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .use(Encode)
+            .use(Decode)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
+
+
